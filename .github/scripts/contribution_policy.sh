3a73199ab67a7bdcc5b2a995a4141e4c0b5cfb89
rumeysakdogan <dgn.rumeysaa@gmail.com>
2023-07-26 12:49:34 -0600

    add: echo commit_message

--- .github/scripts/contribution_policy.sh
+++ .github/scripts/contribution_policy.sh
@@ -3,14 +3,14 @@
 echo "INFO: Checking whether the commit message and PR title adhere to the contribution policies."
 
 # Get the commit message of the current HEAD commit
-commit_message=$(git log -1 --pretty=format:%s)
+commit_message=$1
 
-
+echo $commit_message
 # Check if the commit message or PR title follows the contribution policies
 if [[ ! $commit_message =~ ^(Add|Update|Fix|Remove):[[:space:]]+ ]]; then
     echo "Error: Commit message and PR title should start with either 'Add', 'Update', 'Fix', or 'Remove', in uppercase, followed by a colon and a space."
     echo "Example: 'Add: Implement new feature' or 'Fix: Resolve issue with login form'"
-    exit 1   
+    exit 1
 fi
 
 echo "INFO: Commit message and PR title adhere to the contribution policies."
