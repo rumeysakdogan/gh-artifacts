b4c625bf28cc95002342d53fd2eaa04ce4c35e10
rumeysakdogan <dgn.rumeysaa@gmail.com>
2023-07-26 07:02:01 -0600

    add check for pr_title as well

--- .github/scripts/contribution_policy.sh
+++ .github/scripts/contribution_policy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-set -eou pipefail
-
-echo "INFO: Checking whether the commit message adheres to the contribution policies."
+echo "INFO: Checking whether the commit message and PR title adhere to the contribution policies."
 
 # Get the commit message of the current HEAD commit
 commit_message=$(git log -1 --pretty=format:%s)
 
-# Check if the commit message follows the contribution policies
-if [[ ! $commit_message =~ ^(Add|Update|Fix|Remove):[[:space:]] ]]; then
-    echo "Error: Commit message should start with either 'Add', 'Update', 'Fix', or 'Remove', in uppercase, followed by a colon and a space."
+# Get the PR title from the environment variable (GITHUB_HEAD_REF)
+# This assumes you are using the `pull_request` event, where GITHUB_HEAD_REF holds the PR branch name
+pr_title="${{ github.event.pull_request.title }}"
+
+# Check if the commit message or PR title follows the contribution policies
+if [[ ! $commit_message =~ ^(Add|Update|Fix|Remove):[[:space:]] || ! $pr_title =~ ^(Add|Update|Fix|Remove):[[:space:]] ]]; then
+    echo "Error: Commit message and PR title should start with either 'Add', 'Update', 'Fix', or 'Remove', in uppercase, followed by a colon and a space."
     echo "Example: 'Add: Implement new feature' or 'Fix: Resolve issue with login form'"
     exit 1
 fi
 
-echo "INFO: Commit message adheres to the contribution policies."
+echo "INFO: Commit message and PR title adhere to the contribution policies."
