fd93aeb00b28db8e669e1d0fdb11384f8cf074bf
rumeysakdogan <dgn.rumeysaa@gmail.com>
2023-08-10 16:55:33 -0400

    rm extra linebreak

--- .github/scripts/aug10_move.sh
+++ .github/scripts/aug10_move.sh
@@ -21,16 +21,6 @@ insert_start_line=$((end_threshold_line - 2))
     # ${DEBUG} && echo "end_changed_line: $end_changed_line"
 ${DEBUG} && echo "insert_line: $insert_start_line"
 
-    # first_changed_line=$(git diff -U0 HEAD~1 -- Dockerfile | grep -m 1 -oP "(?<=\+)(\d+)(?=,?\d* @@)")
-    # ${DEBUG} && echo "first_changed_line: $first_changed_line"
-
-    # total_changed_lines=$(git diff -U0 HEAD~1 -- Dockerfile | grep -m 1 -oP "(?<=,)(\d+)(?=,?\d*\s+@@)")
-    # ${DEBUG} && echo "total_changed_lines: $total_changed_lines" 
-    # last_changed_line=$(( first_changed_line + total_changed_lines - 1 ))
-    # ${DEBUG} && echo "last_changed_line: $last_changed_line"
-    # lines_to_insert=$(sed -n "${first_changed_line},${last_changed_line}p" "Dockerfile")
-    # ${DEBUG} && echo -e "lines_to_insert: \n$lines_to_insert"
-
 start_threshold_line_beginning=$((start_threshold_line - 1))
 ${DEBUG} && echo "start_threshold_line_beginning: $start_threshold_line_beginning"
 start_threshold_line_ending=$((start_threshold_line + 5))
@@ -56,40 +46,13 @@ sed -i "${insert_start_line}r /tmp/temp_lines_to_insert.txt" "Dockerfile"
 end_threshold_line=$(grep -n "NEXT RELEASE CHANGES END THRESHOLD" "Dockerfile" | cut -d ':' -f 1)
 linebreak_line=$((end_threshold_line - 1))
 sed -i -e "${linebreak_line}i\\\\n" "Dockerfile"
-    # # Find the line number above the target lines
-    # start_threshold_line_after_move=$(grep -n "# NEXT RELEASE CHANGES START THRESHOLD" "Dockerfile" | cut -d ':' -f1)
-    # ${DEBUG} && echo "start_threshold_line: $start_threshold_line_after_move"
+# Remove extra linebreak 
+end_threshold_line_after=$(grep -n "NEXT RELEASE CHANGES END THRESHOLD" "Dockerfile" | cut -d ':' -f 1)
+linebreak_line_to_remove=$((end_threshold_line - 2))
+sed -i "${linebreak_line_to_remove}d" "Dockerfile"
 
-    # # If the target lines are found, add a blank line above them
-    # if [ -n "$start_threshold_line_after_move" ]; then
-    #     line_number_above=$((start_threshold_line_after_move - 1))  # Line number above the target lines -2
 
-    #     # Add the blank line above the target lines
-    #     sed -i -e "${line_number_above}i\\\\n" "Dockerfile"
-    #     #sed -i "${start_threshold_line_after_move}i\\n" "Dockerfile" did not work
-    #     #sed -i '${start_threshold_line_after_move}N;i\' "Dockerfile" did not work
-    #     #sed -i -e "${start_threshold_line_after_move}i\\\\n" "Dockerfile" # adds double linebreak
-    #     #sed -i -e "${start_threshold_line_after_move}i\&\"" "Dockerfile" did not work
-    #     #sed "${start_threshold_line_after_move}s/$/\n/" did not work
-    #     #sed -i "${start_threshold_line_after_move}a\\" "Dockerfile" did not work
-
-echo "::group:: print Dockerfile"
-cat Dockerfile
-echo "::endgroup::"
-    #     start_threshold_line_after_move=$(grep -n "# NEXT RELEASE CHANGES START THRESHOLD" "Dockerfile" | cut -d ':' -f1)
-    #     ${DEBUG} && echo "start_threshold_line after adding double linebreak: $start_threshold_line_after_move"
-    #     line_number_above=$((start_threshold_line_after_move - 2))
-
-    #     sed -i "${line_number_above}d" "Dockerfile"
-    #     start_threshold_line_after_move=$(grep -n "# NEXT RELEASE CHANGES START THRESHOLD" "Dockerfile" | cut -d ':' -f1)
-    #     ${DEBUG} && echo "start_threshold_line after deleting extra linebreak from above start: $start_threshold_line_after_move"
-    #     line_number_below=$((start_threshold_line_after_move + 6))
-    #     sed -i "${line_number_below}d" "Dockerfile"
-    # else
-    #     echo "Target lines not found in the file."
-    # fi
-    
-    # Clean up the temporary file
+# Clean up the temporary file
 rm /tmp/temp_lines_to_insert.txt
 # fi
 # exit 0
